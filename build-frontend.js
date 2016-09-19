e4caaef08c6205690cbed9431446e6ce160c1470
Shaun Taylor <shaunmtaylor@gmail.com>
2016-09-19 16:19:24 +0100

    Flask app serves arbitrary json file. Functions to convert from json to jsonp and back again. Added jsonp conversion to build-frontend.js.

--- build-frontend.js
+++ build-frontend.js
@@ -5,18 +5,23 @@ var program = require('commander');
 var fs = require('fs');
 var watch = require('watch');
 var path = require('path');
- 
+var release = require(path.join(__dirname, 'release_json', 'test_conversion.json'));
+
 program
   .option('-w --watch')
   .parse(process.argv);
 
-template_folder = path.join(__dirname, 'templates')
+template_folder = path.join(__dirname, 'templates');
+example_folder = path.join(__dirname, 'example');
 
-opts = {"include": ['\\.html$', '\\.jinja$']}
+release_jsonp = 'example_release = ' + JSON.stringify(release, null, 2);
+
+opts = {"include": ['\\.html$', '\\.jinja$']};
 fs.writeFile(path.join(__dirname, 'templates.js'), nunjucks.precompile(template_folder, opts))
-
+fs.writeFile(path.join(example_folder, 'release.jsonp'), release_jsonp);
 if (program.watch) {
    watch.watchTree(template_folder, {'interval': 0.1}, function (f, curr, prev) {
-     fs.writeFile(path.join(__dirname, 'templates.js'), nunjucks.precompile(template_folder, opts))
-   })
+     fs.writeFile(path.join(__dirname, 'templates.js'),
+       nunjucks.precompile(template_folder, opts))
+   });
 }
--- jinja_render.py
+++ jinja_render.py
@@ -1,14 +1,49 @@
 from jinja2 import Environment, FileSystemLoader
 from flask import Flask
 import imp
-from pprint import pprint
+import json
+from tempfile import NamedTemporaryFile
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def root():
-    release_json = imp.load_source('release_json', 'example/release.json')
+def convert_jsonp_to_json(jsonp_file, json_file):
+    """ Convert a JSONP file of the format:
+    example_release = /* JS object representing release */
+    into a valid JSON file
+
+    @param jsonp_file - path of JSONP file
+    @param json_file - path of JSON file
+    """
+    with open(jsonp_file) as f:
+        jsonp_contents = f.read()
+
+    temp_file = NamedTemporaryFile(delete=False)
+    temp_file.write('null = None\nfalse = False\n' + jsonp_contents)
+    temp_file.close()
+
+    jsonp = imp.load_source('jsonp', temp_file.name)
+
+    with open(json_file, 'w+') as f:
+        f.write(json.dumps(jsonp.example_release, indent=4))
+
+    os.unlink(temp_file.name)
+
+
+def load_json(json_file):
+    """Load a JSON file as a Python object
+
+    @param json_file - relative path of file containing valid JSON
+    @return Python object
+    """
+    with open(json_file) as f:
+        json_obj = f.read()
+    return json.loads(json_obj)
+
+
+@app.route('/<release_file>')
+def serve_release(release_file):
     env = Environment()
     env.loader = FileSystemLoader('templates')
 
@@ -17,7 +52,7 @@ def root():
     env.filters['gettext'] = lambda text: text
 
     templ = env.get_template('base_html.html')
-    return templ.render(release_json.example_release)
+    return templ.render(load_json('release_json/{0}'.format(release_file)))
 
 
 if __name__ == "__main__":
